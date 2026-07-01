5aae7dbe7ec8773a749470092bdd5f002faac833
aymane3333 <aimcne01@gmail.com>
2026-07-01 18:13:37 +0200

    derniere chose

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,16 +1,16 @@
 import Route from "./Route.js";
 
-//Définir ici vos routes
+// Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin","conexion","/pages/auth/signin.html"),
-    new Route("/signup","Inscription","/pages/auth/signup.html","/js/auth/signup.js"),
-    new Route("/account","Mon compte","/pages/auth/account.html"),
-    new Route("/editPassword","Changement de mot de passe","/pages/auth/editPassword.html"),
-    new Route("/allResa","Réserver","/pages/auth/reservations/allResa.html"),
-    new Route("/reserver","Réserver","/pages/auth/reservations/reserver.html"),
+    new Route("/", "Accueil", "pages/home.html"),
+    new Route("/galerie", "La galerie", "pages/galerie.html"),
+    new Route("/signin", "conexion", "pages/auth/signin.html", "js/auth/signin.js"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html", "js/auth/signup.js"),
+    new Route("/account", "Mon compte", "pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
+    new Route("/allResa", "Réserver", "pages/auth/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "pages/auth/reservations/reserver.html"),
 ];  
 
-//Le titre s'affiche comme ceci : Route.titre - websitename
+// Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
--- Router/router.js
+++ Router/router.js
@@ -2,18 +2,16 @@ import Route from "./Route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html");
+const route404 = new Route("404", "Page introuvable", "pages/404.html");
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
   let currentRoute = null;
-  // Parcours de toutes les routes pour trouver la correspondance
   allRoutes.forEach((element) => {
     if (element.url == url) {
       currentRoute = element;
     }
   });
-  // Si aucune correspondance n'est trouvée, on retourne la route 404
   if (currentRoute != null) {
     return currentRoute;
   } else {
@@ -24,25 +22,17 @@ const getRouteByUrl = (url) => {
 // Fonction pour charger le contenu de la page
 const LoadContentPage = async () => {
   const path = window.location.pathname;
-  // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
-  // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
-  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
   document.getElementById("main-page").innerHTML = html;
 
-  // Ajout du contenu JavaScript
   if (actualRoute.pathJS != "") {
-    // Création d'une balise script
     var scriptTag = document.createElement("script");
     scriptTag.setAttribute("type", "text/javascript");
     scriptTag.setAttribute("src", actualRoute.pathJS);
-
-    // Ajout de la balise script au corps du document
     document.querySelector("body").appendChild(scriptTag);
   }
 
-  // Changement du titre de la page
   document.title = actualRoute.title + " - " + websiteName;
 };
 
@@ -50,15 +40,18 @@ const LoadContentPage = async () => {
 const routeEvent = (event) => {
   event = event || window.event;
   event.preventDefault();
-  // Mise à jour de l'URL dans l'historique du navigateur
-  window.history.pushState({}, "", event.target.href);
-  // Chargement du contenu de la nouvelle page
+  
+  const href = event.target.closest('a').href;
+  window.history.pushState({}, "", href);
   LoadContentPage();
+
+  // Fermer le menu hamburger après le clic
+  const navbarCollapse = document.getElementById("navbarSupportedContent");
+  if (navbarCollapse.classList.contains("show")) {
+    navbarCollapse.classList.remove("show");
+  }
 };
 
-// Gestion de l'événement de retour en arrière dans l'historique du navigateur
 window.onpopstate = LoadContentPage;
-// Assignation de la fonction routeEvent à la propriété route de la fenêtre
 window.route = routeEvent;
-// Chargement du contenu de la page au chargement initial
 LoadContentPage();
